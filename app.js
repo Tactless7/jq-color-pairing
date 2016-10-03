cfc22fdcf160f95a26fc5f486fbae8c117de5419
Juliane Blier <blier.juliane@gmail.com>
2016-10-03 16:24:43 +0200

    Résolution de l'exercice

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-// good luck !
+var inputs = $('.color');
+//console.log(inputs);
+var len = inputs.length;
+//console.log(len);
+
+for (var i = 0; i < len; i++) {
+	// console.log('kikou' + i);
+	var color = $(inputs[i]).data('color');
+	// console.log(color);
+	$(inputs[i]).css('background-color', color);
+}
+
+$('.color').click(function(){
+	var bgCol = $(this).data('color');
+	console.log(bgCol);
+	if ($('#modify-texte').is(':checked')){
+		$('.main').css('color', bgCol);
+	} else {
+		$('body').css('background-color', bgCol);
+	}
+});
